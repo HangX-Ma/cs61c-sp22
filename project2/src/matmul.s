f9f899b0eada33d5923df120fa384cb19408b545
MContour <m-contour@qq.com>
2023-03-28 21:47:17 +0800

    matmul finished

--- project2/src/matmul.s
+++ project2/src/matmul.s
@@ -13,13 +13,14 @@
 #   a5 (int)   is the # of columns (width) of m1
 #   a6 (int*)  is the pointer to the the start of d
 # Registers:
-#   t0, outer counter
+#   s0, column number
+#   t0, row number
 #   t1, inner counter
-#   t2, outer, memory address
-#   t3, inner, memory address
+#   t2, outer, memory pointer
+#   t3, inner/outer, memory address
 #   t4, outer element
 #   t5, inner element
-#   t6, total element counter
+#   t6, vec sum
 # Returns:
 #   None (void), sets d = matmul(m0, m1)
 # Exceptions:
@@ -40,42 +41,62 @@ matmul:
     blt     a5, t0, error
     bne     a2, a4, error
     # Prologue
-    mv      t0, x0 # set outer counter to zero
-    mv      t6, x0 # total element
+    addi    sp, sp, -4
+    sw      s0, 0(sp)
+    # init
+    mv      t0, x0 # set row number to zero
+    mv      s0, x0 # set column number to zero
+    mv      t6, x0 
 outer_loop:
     mv      t1, x0 # set inner counter to zero
-    mul     t2, t2, a2 # get outer memory bias
+    mv      t6, x0 # sum
+    mul     t2, a2, t0 # get row memory bias
     slli    t2, t2, 2 # 4 bytes per element
     add     t2, a0, t2 # get outer pointer address
 inner_loop:
-    # access inner element
-    mul     t3, a4, t1 # step stride
+    # access m1 element
+    mul     t3, a5, t1 # step stride
+    add     t3, t3, s0 # column bias added
     slli    t3, t3, 2 # 4 bytes per element
     add     t3, a3, t3 # get inner element address
     lw      t5, 0(t3)
-    # access outer element
+    # access m0 element
     slli    t3, t1, 2 # 4 bytes per element
     add     t3, t2, t3 # get outer element address
     lw      t4, 0(t3)
     mul     t4, t5, t4 # store result in t4
     # store element
-    addi    t6, t6, # TODO : NEED STRIDE
-    slli    t6, t6, 2
-    addi    t5, t6, a6
-
+    add     t6, t6, t4
     # check inner loop condition
     addi    t1, t1, 1
     bge     t1, a2, inner_end
-
+    j       inner_loop
 inner_end:
+    # get bias for output pointer
+    mul     t4, a5, t0
+    slli    t4, t4, 2
+    # get current access memory address
+    slli    t5, s0, 2
+    add     t5, a6, t5
+    add     t5, t5, t4 # add bias
+    sw      t6, 0(t5)
+    # reset temp sum
+    mv      t6, x0
+    # check if need clear column number
+    addi    s0, s0, 1
+    bge     s0, a5, clear_col_num
+    j       outer_loop
+clear_col_num:
+    mv      s0, x0
+outer_end:
     # check outer loop condition
     addi    t0, t0, 1
-    bge     t0, a1, outer_end
+    bge     t0, a1, out
     j       outer_loop
-outer_end:
-    # Epilogue
+out:
+    lw      s0, 0(sp)
+    addi    sp, sp, 4
     ret
-
 error:
     li      a0, 38
     j       exit
